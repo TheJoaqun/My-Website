05349d52480f579000b493b4e1bcf3f6aa85e3fb
TheJoaqun <joaquinyang2300@gmail.com>
2023-05-24 20:09:00 -0300

    fix: :hammer: Some Fix Added, And Added Video Lopp And Pop Out Animation Ect..

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("fire");
 button.addEventListener("click", () => {
-  document.getElementById("myPopup").classList.toggle("show");
+  document.getElementById("byVideo").classList.toggle("show");
   document.getElementById("fire").classList.toggle("btn-hidden");
-  document.getElementById("myPopup").play();
+  document.getElementById("byVideo").play();
 });
